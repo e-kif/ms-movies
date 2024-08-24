9551e4c3e9fb79c6be75896d9c38041ff0381320
e-kif <yevhen>
2024-08-24 22:32:11 +0200

    MovieApp clas is implemented. Movies v1.0 is fully converted to OOP

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from storage_json import StorageJson
+from movie_app import MovieApp
 
 
 def main():
     storage = StorageJson('data.json')
-    print(storage.list_movies())
-    storage.add_movie('Nobody', 2021, 7.4, "https://m.media-amazon.com/images/M/MV5BMjM5YTRlZmUtZGVmYi00ZjE2LWIyNzAtOWVhMDk1MDdkYzhjXkEyXkFqcGdeQXVyMjMxOTE0ODA@._V1_FMjpg_UY5000_.jpg")
-    print(storage.list_movies())
+    movie_app = MovieApp(storage)
+    movie_app.run()
+
 
 if __name__ == "__main__":
     main()

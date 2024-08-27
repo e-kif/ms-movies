d615aa741882558fcbd92388fcc2b0afddeccc12
e-kif <yevhen>
2024-08-27 22:37:21 +0300

    main.py can take command line arguments

--- main.py
+++ main.py
@@ -1,11 +1,26 @@
+import argparse
+import os
 from storage.storage_json import StorageJson
+from storage.storage_csv import StorageCsv
 from movie_app import MovieApp
 
 
 def main():
-    """Creates instances of class StorageJson or Storage CSV and MovieApp, runs MovieApp"""
-    storage = StorageJson('data/data.json')
-    # storage = StorageCsv('data.json')
+    """Reads passed commandline argument, create instance of class StorageJson or StorageCsv,
+    MovieApp and runs MovieApp
+    """
+    parser = argparse.ArgumentParser()
+    parser.add_argument('echo')
+    filename_from_argument = parser.parse_args().echo
+    if filename_from_argument.endswith('.csv'):
+        storage = StorageCsv(filename_from_argument)
+    elif filename_from_argument.endswith('.json'):
+        storage = StorageJson(filename_from_argument)
+    else:
+        filename = os.path.join("data", "data.csv")
+        print(f'No valid filename is provided. Proceeding with default database "{filename}"')
+        storage = StorageCsv(filename)
+
     movie_app = MovieApp(storage)
     movie_app.run()
 
